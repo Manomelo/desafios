4ea0f1fa786d0ed96a281d93519bfac947628ecb
manomelo <lucas.melo2103@gmail.com>
2026-03-16 17:53:48 -0300

    feat(model): criado o modelo de dados para transacao

---
+++ desafio-itau/src/main/java/desafio/backend/itau/model/Transacao.java
@@ -0,0 +1,18 @@
+package desafio.backend.itau.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transacao {
+
+    private double valor;
+    private OffsetDateTime dataHora;
+}
