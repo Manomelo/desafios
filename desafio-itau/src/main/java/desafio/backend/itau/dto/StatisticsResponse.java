2871fb88165c6825854ca99007808421b084b688
manomelo <lucas.melo2103@gmail.com>
2026-03-16 18:10:58 -0300

    feat(dto): adicionado dto para resposta com status

---
+++ desafio-itau/src/main/java/desafio/backend/itau/dto/StatisticsResponse.java
@@ -0,0 +1,23 @@
+package desafio.backend.itau.dto;
+
+import lombok.Getter;
+
+import java.util.DoubleSummaryStatistics;
+
+@Getter
+public class StatisticsResponse {
+
+    private long count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+
+    public StatisticsResponse(DoubleSummaryStatistics stats){
+        this.count = stats.getCount();
+        this.sum = stats.getSum();
+        this.avg = stats.getAverage();
+        this.min = stats.getMin();
+        this.max = stats.getMax();
+    }
+}
