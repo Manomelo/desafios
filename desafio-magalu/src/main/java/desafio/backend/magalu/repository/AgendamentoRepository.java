fef4fc2d8783480d561327c78cc37c32ef712d80
manomelo <lucas.melo2103@gmail.com>
2026-03-16 20:34:09 -0300

    feat(repository): criado o repository para gerenciar agendamentos

---
+++ desafio-magalu/src/main/java/desafio/backend/magalu/repository/AgendamentoRepository.java
@@ -0,0 +1,9 @@
+package desafio.backend.magalu.repository;
+
+import desafio.backend.magalu.model.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+}
