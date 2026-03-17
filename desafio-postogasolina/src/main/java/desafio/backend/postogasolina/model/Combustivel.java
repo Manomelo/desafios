9313578b602ae2b4457e3ee77d16f85736747fc4
manomelo <lucas.melo2103@gmail.com>
2026-03-17 10:46:58 -0300

    feat(model): criado o modelo de dados para combustivel

---
+++ desafio-postogasolina/src/main/java/desafio/backend/postogasolina/model/Combustivel.java
@@ -0,0 +1,25 @@
+package desafio.backend.postogasolina.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Combustivel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Double precoLitro;
+}
